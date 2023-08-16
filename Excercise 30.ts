4f0f76fed94a8e6a496431c5b7f9c8355bcf3af1
Saqlain788 <139763515+Saqlain788@users.noreply.github.com>
2023-08-16 09:29:12 -0700

    initialize

---
+++ Excercise 30.ts
@@ -0,0 +1,13 @@
+/*Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:
+• If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
+• Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again*/
+
+let userNames: string[] = ["Admin", "Saqlain", "Husnain", "Haider", "Adeel"];
+for (let i = 0; i < userNames.length; i++) {
+  let username = userNames[i];
+  if (username === "Admin") {
+    console.log("Hello Admin, would you like to see a status report?");
+  } else {
+    console.log(`Hello ${username}, thak you for loggin in again`);
+  }
+}
