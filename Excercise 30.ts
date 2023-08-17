0edb87069f93c190535f9098064f7a67f363c127
Saqlain788 <saqlain788@gmail.com>
2023-08-17 11:24:26 +0500

    initialize

--- Excercise 30.ts
+++ Excercise 30.ts
@@ -2,7 +2,13 @@
 • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
 • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again*/
 
-let userNames: string[] = ["Admin", "Saqlain", "Husnain", "Haider", "Adeel"];
+export let userNames: string[] = [
+  "Admin",
+  "Saqlain",
+  "Husnain",
+  "Haider",
+  "Adeel",
+];
 for (let i = 0; i < userNames.length; i++) {
   let username = userNames[i];
   if (username === "Admin") {
