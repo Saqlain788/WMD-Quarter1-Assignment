2929f2fbbd15101260b5cb1a68abe8f44327042c
Saqlain788 <saqlain788@gmail.com>
2023-08-22 10:21:11 +0500

    initialize

--- Excercise 36.js
+++ Excercise 36.js
@@ -1,6 +1,6 @@
 /*T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.*/
 function make_shirt(size, message) {
-    console.log(` I have order the shirt sized ${size} of brand name ${message} printed on it`);
+    console.log(` I have ordered the shirt sized ${size} of brand name ${message} printed on it`);
 }
 make_shirt("Medium", "Leisure Club");
 export {};
