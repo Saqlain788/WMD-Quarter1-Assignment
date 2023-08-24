ee5d5f5daffb4381eabc9768b460f063b4f8aebf
Saqlain788 <saqlain788@gmail.com>
2023-08-24 15:23:17 +0500

    2nd commit

--- Excercise 10.ts
+++ Excercise 10.ts
@@ -1,8 +1,11 @@
 // Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
 // solution:-
 // first step: single line comment can be done with double slash (//) before statement as done below:-
-//console.log(4+4);
-//console.log(12-4);
-// Whereas Step 2: Multilines commments is done (/* statements */) as below:-
-/*console.log(2*4);   
-console.log(32/4);*/
+
+//This program take two number and return their sum
+function sum(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+console.log(sum(7, 9));
