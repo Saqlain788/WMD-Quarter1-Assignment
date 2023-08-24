3c592a3bd95c65bb2205e9317a68c8fd3946c3b9
Saqlain788 <saqlain788@gmail.com>
2023-08-24 13:48:16 +0500

    commit

--- Excercise 43.ts
+++ Excercise 43.ts
@@ -1,14 +1,17 @@
 /*Unchanged Magicians: Start with your work from Exercise 40. 
-Call the function make_great() with a copy of the array of magicians’ names. Because the original array will be unchanged, return the new array and store it in a separate array. 
-Call show_magicians() with each array to show that you have one array of the original names and one array with the Great added to each magician’s name.*/
+2. Call the function make_great() with a copy of the array of magicians’ names. Because the original array will be unchanged, return the new array and store it in a separate array. 
+3. Call show_magicians() with each array to show that you have one array of the original names and one array with the Great added to each magician’s name.*/
+// Step 1. 
 import { show_magicians, Magicians } from "./Excercise 41.js";
 import { make_great } from "./Excercise 42.js";
 
+// Step 2.
 const originalMagicianNames = [...Magicians]; // create a copy of magician array with spread operator.
 console.log("originalMagicianNames:", originalMagicianNames);
 
 const greatMagicianNames = make_great(originalMagicianNames);
 console.log("greatMagicianNames:", greatMagicianNames);
-// call show-magician with both array showing that original magician and great magician.
+
+// Step 3. call show-magician with both array showing that original magician and great magician.
 show_magicians(originalMagicianNames);
 show_magicians(greatMagicianNames);
---
+++ Excercise 44.ts
@@ -0,0 +1,16 @@
+/* Sandwiches: Write a function that accepts an array of items a person wants on a sandwich. 
+The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that is being ordered. 
+Call the function three times, using a different number of arguments each time.*/
+
+// step 1. make the function sandwithItems()
+function sandwithItems(item: string[]): void {
+  console.log("Sandwitch Ordered:", item.join(" : "));
+}
+
+const item1: string[] = ["Chiken", "Vegetable", "Meat"];
+const item2: string[] = ["Club", "BBQ", "Fish"];
+const item3: string[] = ["Potato", "Egg Salad", "Creamy"];
+
+sandwithItems(item1);
+sandwithItems(item2);
+sandwithItems(item3);
