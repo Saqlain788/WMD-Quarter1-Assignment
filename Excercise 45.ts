9e16d9de4972f989d1b542dfd2c95038bc475604
Saqlain788 <saqlain788@gmail.com>
2023-08-28 14:01:33 +0500

    initialize

--- Excercise 45.ts
+++ Excercise 45.ts
@@ -2,3 +2,13 @@
 The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. 
 Call the function with the required information and two other name-value pairs, such as a color or an optional feature. 
 Print the Object that’s returned to make sure all the information was stored correctly.*/
+
+const car_object = (manufacturer: string, model: string, ...args: any) => {
+  let car = { manufacturer, model };
+  Object.entries(args).map(([key, value]) => {
+    car = { ...car, ...(value as Object) };
+  });
+  return car;
+};
+console.log(car_object("Hyundai", "Elentra"));
+console.log(car_object("Toyota", "Fortuner", { color: "black" }, { seats: 7 }));
